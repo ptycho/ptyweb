40657ffb078ac7782ae852578cf2a74b93ce4c59
Thomas Milburn <thomas.milburn@diamond.ac.uk>
2022-12-08 15:28:03 +0000

    Updated frontend to accept files and fetch reconstruction data from backend

--- frontend/src/components/Configurator/Configurator.tsx
+++ frontend/src/components/Configurator/Configurator.tsx
@@ -1,14 +1,52 @@
 import React from 'react';
 import './Configurator.css'
 import VisualizerSection from "../VisualizerSection/VisualizerSection";
+import {DefaultService, ReconstructionViewerResponse} from "../../network";
 
 const Configurator = () => {
-    return (
-      <div>
-        <VisualizerSection/>
-      </div>
-    );
+  const ptyrFile = React.useRef<File | undefined>(undefined)
+  const [reconstructionData, setReconstructionData] = React.useState<ReconstructionViewerResponse | undefined>(undefined)
+  const [message, setMessage] = React.useState<string | undefined>(undefined)
+
+  const onPtyrFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      ptyrFile.current = e.target.files[0]
+    } else {
+      ptyrFile.current = undefined
+    }
   }
-;
+
+  const fetchReconstructionData = () => {
+    if (ptyrFile.current) {
+      // Getting file blob
+      DefaultService.getViewerData({file: ptyrFile.current}).then((res) => {
+        setReconstructionData(res)
+      })
+    } else {
+      setMessage("Please select a file")
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <label>
+          Select ptyr file
+          <input type="file" accept=".ptyr" onChange={onPtyrFileChange}/>
+        </label>
+
+        <button onClick={fetchReconstructionData}>Submit</button>
+
+        {message && <span>{message}</span>}
+      </div>
+
+      {
+        reconstructionData && (
+          <VisualizerSection data={reconstructionData}/>
+        )
+      }
+    </div>
+  );
+};
 
 export default Configurator;
--- frontend/src/components/VisualizerSection/VisualizerSection.tsx
+++ frontend/src/components/VisualizerSection/VisualizerSection.tsx
@@ -1,83 +1,19 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {ApiError, DefaultService, ReconstructionViewerResponse} from "../../network";
+import React from 'react';
 import Visualizer from "../Visualizer/Visualizer";
 import "./VisualizerSection.css"
 import VisualizerGraph from "../VisualizerGraph/VisualizerGraph";
-import {debug} from "util";
+import {ReconstructionViewerResponse} from "../../network";
 
-const VisualizerSection = () => {
-  const [status, setStatus] = useState<string | undefined>(undefined)
-  const [data, setData] = useState<ReconstructionViewerResponse | undefined>(undefined)
-  const dataRef = useRef(data)
-  const updateDataTimeout = useRef<number | undefined>(undefined)
+interface Props {
+  data: ReconstructionViewerResponse
+}
 
-  const updateData = () => {
-    DefaultService.getViewerData({file: new Blob()}).then((res) => {
-      setStatus(undefined)
-      setData(res)
-      dataRef.current = res
-    }).catch((err) => {
-      // Check if an API error
-      if (err instanceof ApiError) {
-        if (err.status === 404 && err.body.detail === "The cluster job has already finished") {
-          // The job has finished, just keep the data from last successful request if it exists
-          debugger
-          if (dataRef.current !== undefined) {
-            return;
-          }
-        }
-
-        setData(undefined)
-        dataRef.current = undefined
-
-        if (err.status === 403) {
-          setStatus(undefined) // Hiding 403 status (form not submitted yet, so we don't have a bearer)
-          return
-        }
-
-        if (err.body.detail) {
-          setStatus(err.body.detail)
-          return
-        }
-
-        setStatus(err.body + "")
-      } else {
-        setData(undefined)
-        dataRef.current = undefined
-        setStatus(err.message)
-      }
-    }).finally(() => {
-      updateDataTimeout.current = window.setTimeout(updateData, 5000)
-    })
-  }
-
-  useEffect(() => {
-    updateData()
-
-    return () => {
-      // Cleanup
-      if (updateDataTimeout.current) {
-        window.clearTimeout(updateDataTimeout.current)
-      }
-    }
-  }, [])
+const VisualizerSection = ({data}: Props) => {
   return (
-    <div>
-      {
-        status && (
-          <span>{status}</span>
-        )
-      }
-
-      {
-        data && (
-          <div className={"visualizerSection"}>
-            <Visualizer data={data.object} title={"Object"}/>
-            <Visualizer data={data.probe} title={"Probe"}/>
-            <VisualizerGraph data={data.graph}/>
-          </div>
-        )
-      }
+    <div className={"visualizerSection"}>
+        <Visualizer data={data.object} title={"Object"}/>
+        <Visualizer data={data.probe} title={"Probe"}/>
+        <VisualizerGraph data={data.graph}/>
     </div>
   );
 };
