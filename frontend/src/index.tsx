7452497b94734adc2fc285e4553be89746f4f330
Thomas Milburn <thomas.milburn@diamond.ac.uk>
2022-12-08 15:43:25 +0000

    Removed redux

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,20 +2,14 @@ import React from 'react';
 import {render} from 'react-dom'
 import './index.css';
 import App from './App';
-import {Provider} from "react-redux";
-import {store} from "./redux/store";
 import ReactModal from "react-modal";
 
 const rootDomElement = document.getElementById('root') as HTMLElement
 
-// const root = ReactDOM.createRoot(rootDomElement);
-//
 ReactModal.setAppElement(rootDomElement)
 
 render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App/>
-    </Provider>
+    <App/>
   </React.StrictMode>, rootDomElement
 );
