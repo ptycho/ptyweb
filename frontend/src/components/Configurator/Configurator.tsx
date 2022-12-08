a669df1f04b557303330ee04184b868ab5ff6e6a
Thomas Milburn <thomas.milburn@diamond.ac.uk>
2022-12-08 15:32:14 +0000

    Prevented form being submitted whilst already submitting and when a file has not been selected

--- frontend/src/components/Configurator/Configurator.tsx
+++ frontend/src/components/Configurator/Configurator.tsx
@@ -7,23 +7,32 @@ const Configurator = () => {
   const ptyrFile = React.useRef<File | undefined>(undefined)
   const [reconstructionData, setReconstructionData] = React.useState<ReconstructionViewerResponse | undefined>(undefined)
   const [message, setMessage] = React.useState<string | undefined>(undefined)
+  const [allowSubmit, setAllowSubmit] = React.useState<boolean>(false)
 
   const onPtyrFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       ptyrFile.current = e.target.files[0]
+      setAllowSubmit(true)
     } else {
       ptyrFile.current = undefined
+      setAllowSubmit(false)
     }
   }
 
   const fetchReconstructionData = () => {
     if (ptyrFile.current) {
       // Getting file blob
+      setMessage("Submitting file...")
+      setAllowSubmit(false)
       DefaultService.getViewerData({file: ptyrFile.current}).then((res) => {
         setReconstructionData(res)
+        setMessage(undefined)
+      }).finally(() => {
+        setAllowSubmit(true)
       })
     } else {
       setMessage("Please select a file")
+      setAllowSubmit(false)
     }
   }
 
@@ -35,7 +44,7 @@ const Configurator = () => {
           <input type="file" accept=".ptyr" onChange={onPtyrFileChange}/>
         </label>
 
-        <button onClick={fetchReconstructionData}>Submit</button>
+        <button onClick={fetchReconstructionData} disabled={!allowSubmit}>Submit</button>
 
         {message && <span>{message}</span>}
       </div>
