576c6a7146cf1180e7031632ac6ce5879383b7ca
Thomas Milburn <thomas.milburn@diamond.ac.uk>
2022-12-13 10:15:43 +0000

    Added pyweb components to story book

---
+++ packages/lib/src/stories/Visualizer.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import Visualizer from "../components/Visualizer/Visualizer";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Visualization/Visualizer',
+  component: Visualizer
+} as ComponentMeta<typeof Visualizer>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Visualizer> = (args) => (
+  <Visualizer
+    {...args}
+  />
+);
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  title: "Example reconstruction",
+  data: {
+    imaginary: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+    real: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+    pixel_size: 1
+  }
+};
---
+++ packages/lib/src/stories/VisualizerGraph.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import VisualizerGraph from "../components/VisualizerGraph/VisualizerGraph";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Visualization/VisualizerGraph',
+  component: VisualizerGraph
+} as ComponentMeta<typeof VisualizerGraph>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof VisualizerGraph> = (args) => (
+  <VisualizerGraph
+    {...args}
+  />
+);
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  data: {
+    fmag: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
+    phot: [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.9, 0.9],
+    ex: [0.5, 0.6, 0.7, 0.8, 0.9, 0.9, 0.9, 1, 1],
+    iteration_numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  }
+};
