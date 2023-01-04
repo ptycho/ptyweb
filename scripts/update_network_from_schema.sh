d1a7e279e7e96447bf33b71d720bc11a3e14e992
Thomas Milburn <thomas.milburn@diamond.ac.uk>
2023-01-04 14:37:03 +0000

    Updated generate network script to work with new folder structure

--- scripts/update_network_from_schema.sh
+++ scripts/update_network_from_schema.sh
@@ -1,7 +1,7 @@
-node ../frontend/node_modules/openapi-typescript-codegen/bin/index.js -i http://localhost:8000/openapi.json -o network --indent 2
+node ../packages/app/frontend/node_modules/openapi-typescript-codegen/bin/index.js -i http://localhost:8000/openapi.json -o network --indent 2
 cp OpenAPIBackup.ts.backup network/core/OpenAPI.ts
 cp requestBackup.ts.backup network/core/request.ts
-rm -r ../frontend/src/network
-mv network ../frontend/src/network
-git add ../frontend/src/network
+rm -r ../packages/app/frontend/src/network
+mv network ../packages/app/frontend/src/network
+git add ../packages/app/frontend/src/network
 echo "Finished generating new schema"
